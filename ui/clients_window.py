562cf1ec445ace6fd9d9177167f747c615676b40
witkowsky <huntik29@gmail.com>
2025-02-26 00:34:02 +0100

    membership getter to be resolved later

--- ui/clients_window.py
+++ ui/clients_window.py
@@ -3,6 +3,23 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout,
 from PyQt6.QtWidgets import QSpinBox, QLineEdit, QTextEdit, QComboBox, QGridLayout
 from PyQt6.QtGui import QAction 
 from datetime import date
+from sqlalchemy import select
+from sqlalchemy.orm import sessionmaker
+from pathlib import Path
+parent_path = str(Path(__file__).resolve().parent.parent)
+sys.path.append(parent_path)
+sys.path.append(str(Path(__file__).resolve().parent / "models"))
+from db_setup import engine, Membership, membership
+
+
+class MembershipsGetter():
+    def getMemberships():
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        stmt = select(Membership.membership_name)
+        membership_obj = session.scalars(stmt).all()
+        print(membership_obj)
 
 class AddClientPopup(QDialog):
     def __init__(self):
@@ -12,6 +29,8 @@ class AddClientPopup(QDialog):
 
         # add_client_layout = QVBoxLayout()
         add_client_layout = QGridLayout()
+        add_client_layout.setSpacing(0)
+        add_client_layout.setContentsMargins(10, 10, 10, 10)
 
 
         # Inputy do wprowadzania danych klienta
@@ -26,29 +45,52 @@ class AddClientPopup(QDialog):
         # self.label.move(50,50)
         # self.combo_box.setGeometry(50, 50, 150, 30)
 
-      
-        # self.combo_box = QComboBox(self)
+        self.combo_box_rodo = QComboBox(self)
+        add_client_layout.addWidget(self.combo_box_rodo, 2, 0)
+        self.combo_box_rodo.addItems(["Tak", "Nie"])
+
+        #Nieletni
+        self.label = QLabel("Czy klient jest nieletni", self)
+        add_client_layout.addWidget(self.label, 3, 0)
+
+        self.combo_box_underage = QComboBox(self)
+        add_client_layout.addWidget(self.combo_box_underage, 4, 0)
+        self.combo_box_rodo.addItems(["Tak", "Nie"])
 
         #Imię
         self.label = QLabel("Imię", self)
-        add_client_layout.addWidget(self.label, 2, 0)
+        add_client_layout.addWidget(self.label, 5, 0)
 
         # self.label.move(50,50)
 
         self.text_input = QLineEdit(self)
-        add_client_layout.addWidget(self.text_input, 3, 0)
+        add_client_layout.addWidget(self.text_input, 6, 0)
         # self.text_input.setGeometry(50, 70, 200, 30)
 
         #Nazwisko
         self.label = QLabel("Nazwisko", self)
-        add_client_layout.addWidget(self.label, 4, 0)
+        add_client_layout.addWidget(self.label, 7, 0)
         # self.label.move(50,110)
 
         self.text_input = QLineEdit(self)
-        add_client_layout.addWidget(self.text_input, 5, 0)
+        add_client_layout.addWidget(self.text_input, 8, 0)
         # self.text_input.setGeometry(50, 130, 200, 30)
+
+        #Typ karnetu
+        self.label = QLabel("Wybierz typ karnetu", self)
+        add_client_layout.addWidget(self.label, 9, 0)
+
+        self.combo_box_membership = QComboBox(self)
+        add_client_layout.addWidget(self.combo_box_membership, 10, 0)
+        memberships = MembershipsGetter()
+        self.combo_box_rodo.addItems(["Tak", "Nie"])
+
 
 
         self.setLayout(add_client_layout)
+
+
+
+        
 
 
